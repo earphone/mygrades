8aa1a8ec4d3368d6986d1283d3e0d16c1612990d
ejayhi <ejay_hi@yahoo.com>
2012-11-13 11:12:59 +0000

    Join Table Associations between Models
    Course has many Tasks, Tasks has many Users, Users have one Score.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  has_many :tasks
+  has_many :users, :through => :tasks
+  has_many :scores, :through => :users
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+  belongs_to :course
+  has_many :users
+  has_many :scores, :through => :users
 end
